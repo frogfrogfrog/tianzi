43835363e45616249823a036cea2b0611aa25424
frogfrogfrog <421604505@qq.com>
2015-06-13 23:54:42 +0800

    测试一下

--- tianzi/src/com/example/tianzi/MainActivity.java
+++ tianzi/src/com/example/tianzi/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	//dd
+	//测试一下
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
