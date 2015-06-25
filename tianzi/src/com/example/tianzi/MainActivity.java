80e8f35744518a2199af12c36c7bf0c355ee4334
frogfrogfrog <421604505@qq.com>
2015-06-25 15:14:33 +0800

    xwj12:+level package,+3 xml in layout,+tupian  in drawable

--- tianzi/src/com/example/tianzi/MainActivity.java
+++ tianzi/src/com/example/tianzi/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.tianzi;
 
+
+import com.example.tianzi.level.ChoiceViewActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	//测试一下
-	//机房中测试
+
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+//		Intent intent=new Intent(MainActivity.this,ChoiceViewActivity.class);
+//    	startActivity(intent);
 	}
 }
