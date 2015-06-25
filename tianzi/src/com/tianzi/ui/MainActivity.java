9768239d5bdc0c2d50f918247a7bc469816690d6
frogfrogfrog <421604505@qq.com>
2015-06-25 21:10:42 +0800

    修改了MainActivity

--- tianzi/src/com/tianzi/ui/MainActivity.java
+++ tianzi/src/com/tianzi/ui/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		Intent intent=new Intent(MainActivity.this,ChoiceViewActivity.class);
-//    	startActivity(intent);
+		Intent intent=new Intent(MainActivity.this,ChoiceViewActivity.class);
+    	startActivity(intent);
 	}
 }
