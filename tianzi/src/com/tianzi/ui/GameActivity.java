60c686c27e49b0ca5fbc7d7a3f28fb4522bde9bf
Summery <2691264186@qq.com>
2015-07-10 16:49:58 +0800

    Revert "xmr12"
    This reverts commit 9a24c53da3959a2ac56196b094268fb6f33ef8ef.

--- tianzi/src/com/tianzi/ui/GameActivity.java
+++ tianzi/src/com/tianzi/ui/GameActivity.java
@@ -24,15 +24,15 @@ public class GameActivity extends Activity {
 	GridView gridView;
 	GridView gridView2;
 	TextView questionView;
-	char [] word = {
-		'Q','W','E','R','T','Y','O','P',
-		'A','S','D','F','G','H','J','K',
-		'L','Z','X','C','V','B','N','M'
+	String [] word = {
+		"Q","W","E","R","T","Y","O","P",
+		"A","S","D","F","G","H","J","K",
+		"L","Z","X","C","V","B","N","M"
 	};
 	Logic logic;
 	CellData[][] cellMap;
-	ArrayList<CellData> cellList = new ArrayList<CellData>();
-	ResultData resultData;
+	ArrayList<CellData> cellList;
+	
 	
 	
 	@Override
@@ -51,8 +51,8 @@ public class GameActivity extends Activity {
 		questionView = (TextView) findViewById(R.id.game_textview_question);
 		gridView = (GridView) findViewById(R.id.game_gridview);
 		gridView2 =(GridView) findViewById(R.id.game_gridview2);
-		
 		adapter = new Game_gridview_adapter(GameActivity.this,cellList);
+		
 		gridView.setAdapter(adapter);
 		gridView.setOnItemClickListener(new OnItemClickListener(){
 			public void onItemClick(AdapterView<?> parent, View view,
@@ -70,19 +70,8 @@ public class GameActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				if(currentPosition>=0){
-					CellData temp = cellList.get(currentPosition);
-					if(temp.getState()==2&&temp.getState()==1){
-						resultData = logic.enterWord(currentPosition/10, currentPosition%10, word[position]);
-						cellList.get(currentPosition).setWord(word[position]);
-						if(resultData.getIsCorrect()){
-							ArrayList<CellData> ansList = resultData.getCellData();
-							for(CellData ans:ansList){
-								cellList.get(ans.getxAxis()*10+ans.getyAxis()).setWord(ans.getWord());
-								cellList.get(ans.getxAxis()*10+ans.getyAxis()).setState(ans.getState());
-							}
-						}
-					    adapter.notifyDataSetChanged();
-					}
+//					data[currentPosition] = word[position];
+				    adapter.notifyDataSetChanged();
 				}
 				
 			}
