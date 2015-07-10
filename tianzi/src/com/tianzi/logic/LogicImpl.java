2f0a293f438901154d28bca1bdfa10c7164f2de8
frogfrogfrog <421604505@qq.com>
2015-07-10 11:50:56 +0800

    yzx12修复了几个小bug

--- tianzi/src/com/tianzi/logic/LogicImpl.java
+++ tianzi/src/com/tianzi/logic/LogicImpl.java
@@ -59,7 +59,10 @@ public class LogicImpl implements Logic{
 				if(cdList[x][y+i].getState()==2){
 					//说明 第[x][y+i]也填了英文答案
 					temp.add(cdList[x][y+i]);
+				}else if(cdList[x][y+i].getState()==3){
+					//处于横纵问题的交叉点，已经被解答了
 				}else if(cdList[x][y+i].getState()==1){
+				
 					//出现了该题目有格子未填答案的现象
 					flag=0;
 					break;
@@ -72,8 +75,10 @@ public class LogicImpl implements Logic{
 				//说明xy右边的格子都已经填了英文字母答案，现在遍历左边的格子
 				for(int i=1;i<y+1;i++){
 					if(cdList[x][y-i].getState()==2){
-						//说明 第[x][y-i]也填了英文答案
+						//说明 第[x][y-i]也填了英文答案或者这个所处的纵问题被解答了
 						temp.add(cdList[x][y-i]);
+					}else if(cdList[x][y-i].getState()==3){
+						//处于横纵问题的交叉点，已经被解答了
 					}
 					else if(cdList[x][y-i].getState()==1){
 						//出现了该题目有格子未填答案的现象
@@ -98,7 +103,10 @@ public class LogicImpl implements Logic{
 				if(cdList[x+i][y].getState()==2){
 					//说明 第[x+i][y]也填了英文答案
 					temp.add(cdList[x+i][y]);
-				}else if(cdList[x+i][y].getState()==1){
+				}else if(cdList[x+i][y].getState()==3){
+					//处于横纵问题的交叉点，已经被解答了
+				}
+				else if(cdList[x+i][y].getState()==1){
 					//出现了该题目有格子未填答案的现象
 					flag=0;
 					break;
@@ -113,6 +121,8 @@ public class LogicImpl implements Logic{
 					if(cdList[x-i][y].getState()==2){
 						//说明 第[x-i][y]也填了英文答案
 						temp.add(cdList[x-i][y]);
+					}else if(cdList[x-i][y].getState()==3){
+						//处于横纵问题的交叉点，已经被解答了
 					}
 					else if(cdList[x-i][y].getState()==1){
 						//出现了该题目有格子未填答案的现象
@@ -126,13 +136,16 @@ public class LogicImpl implements Logic{
 			}
 			if(flag==1){
 				//该题目的所以格子都填了英文答案。
-				judgeAnsIsCorrect(resultCD, temp);
+				if(judgeAnsIsCorrect(resultCD, temp)){
+					result.setIsCorrect(true);
+					result.setIsEnd(this.judgeIsEnd());
+				}
 			}
 		}
 		if(resultCD.size()==0){
 			//说明要么答案没填完，要么不对
-			int nextX;
-			int nextY;
+			int nextX = 0;
+			int nextY = 0;
 			if(coordinate[x][y][1]>=0 && cdList[x][y+1].getState()==1){
 				nextX=x;
 				nextY=y+1;
@@ -141,8 +154,15 @@ public class LogicImpl implements Logic{
 				nextX=x+1;
 				nextY=y;
 			}
+			result.setNextX(nextX);
+			result.setNextY(nextY);
 		}
+		
+		
 		result.setCellData(resultCD);
+		
+		//最后不管有没有答题正确都要保存一下游戏
+		this.saveGameRecord();
 		return result;
 	}
 	@Override
@@ -151,19 +171,24 @@ public class LogicImpl implements Logic{
 		ld.saveGameRecord();
 	}
 	
-	private void judgeAnsIsCorrect(ArrayList<CellData> resultCD,ArrayList<CellData> temp){
+	private boolean judgeAnsIsCorrect(ArrayList<CellData> resultCD,ArrayList<CellData> temp){
 		int[][][] coordinate=ld.getCoordinate();
 		ArrayList<Character> ansEN=ld.getAnsEN();
 		ArrayList<Character> ansCN=ld.getAnsCN();
 		for(int i=0;i<temp.size();i++){
 			int xAxis=temp.get(i).getxAxis();
 			int yAxis=temp.get(i).getyAxis();
-			if(temp.get(i).getWord()==ansEN.get(coordinate[xAxis][yAxis][0])){
-				//这个格子答案正确
+			if(temp.get(i).getState()==3){
+				//这个格子已经有中文答案了，横纵问题交叉点。。
 			}else{
-				//这个格子答案不正确，情况暂存区temp
-				temp.clear();
-				break;
+				if (temp.get(i).getWord() == ansEN
+						.get(coordinate[xAxis][yAxis][0])) {
+					// 这个格子答案正确
+				} else {
+					// 这个格子答案不正确，情况暂存区temp
+					temp.clear();
+					break;
+				}
 			}
 		}
 		if(temp.size()>0){
@@ -175,7 +200,9 @@ public class LogicImpl implements Logic{
 				temp.get(i).setWord(ansCN.get(coordinate[xAxis][yAxis][0]));
 			}
 			resultCD.addAll(temp);
+			return true;
 		}
+		return false;
 	}
 	@Override
 	public boolean[] levelIsChieved() {
@@ -183,5 +210,19 @@ public class LogicImpl implements Logic{
 		boolean[] result=new boolean[27];
 		result[0]=true;
 		return result;
+	}
+	
+	
+	private boolean judgeIsEnd(){
+		CellData[][] cdList=ld.getUserAns();
+		for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+				if(cdList[i][j].getState()==1 || cdList[i][j].getState()==2){
+					//这个格子未填或者只有英文答案，这说明该题目未结束
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
--- tianzi/src/com/tianzi/logic/ResultData.java
+++ tianzi/src/com/tianzi/logic/ResultData.java
@@ -9,19 +9,54 @@ import java.util.ArrayList;
  */
 public class ResultData {
 	//游戏是否结束
-	private int isEnd;
+	private boolean isEnd;
+	
+	//答题是否使某一个题目被成功解答，如果是，要取出CellData中的数据进行更新
+	private boolean isCorrect;
+
 	//如果用户输入一个字，解出了某个题目，则返回该题目的所有格子信息。UI根据该信息进行翻转，显示中文字。
 	private ArrayList<CellData> cellData;
 	
+	//如果该问题还有要输入的格子，返回下个格子的位置
+	private int nextX;
+	private int nextY;
+	
+	
+	
+	
+	public boolean getIsCorrect() {
+		return isCorrect;
+	}
+
+	public void setIsCorrect(boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+
+	public int getNextX() {
+		return nextX;
+	}
+
+	public void setNextX(int nextX) {
+		this.nextX = nextX;
+	}
+
+	public int getNextY() {
+		return nextY;
+	}
+
+	public void setNextY(int nextY) {
+		this.nextY = nextY;
+	}
+
 	public ResultData(){
 		
 	}
 
-	public int getIsEnd() {
+	public boolean getIsEnd() {
 		return isEnd;
 	}
 
-	public void setIsEnd(int isEnd) {
+	public void setIsEnd(boolean isEnd) {
 		this.isEnd = isEnd;
 	}
 
@@ -32,4 +67,7 @@ public class ResultData {
 	public void setCellData(ArrayList<CellData> cellData) {
 		this.cellData = cellData;
 	}
+	
+	
+	
 }
